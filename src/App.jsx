fe1d070e47ea51dfc41b8edb575b48fdc75f0c1c
Zeyad <zeyadhyman@gmail.com>
2023-12-30 17:54:14 +0200

    edit nav Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Nav from "./components/Nav";
-import Footer from "./components/Footer";
+import Nav from "./Components/Navbar";
+import Footer from "./Components/Footer";
 function App() {
 	return (
 		<BrowserRouter>
