a45027df6facc281d240ca2fc08c039329841001
Zeyad <zeyadhyman@gmail.com>
2023-12-30 16:26:52 +0200

    1- moved Components folder to src folder 2- added Component(Nav, footer) to Components folder 3- used react-rourter-dom to display Components

---
+++ src/Components/footer.jsx
@@ -0,0 +1,10 @@
+
+function footer() {
+	return (
+		<>	
+			<h1>Hello footer</h1>
+		</>
+	);
+}
+
+export default footer;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import Nav from "./Components/Nav";
+import Footer from "./Components/Footer";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+ <React.StrictMode>
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+ </React.StrictMode>
+);
