cadd36e2853221b26030a23ac1a237b3e9d92b01
AhmadOthman <99755295+AhmedOsman101@users.noreply.github.com>
2023-12-30 17:16:04 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 function App() {
 	return (
-		<>
-			<h1>Metro Mart</h1>
-		</>
+		<BrowserRouter>
+			<Nav />
+			<Routes>
+				<Route path="/" element={<App />} />
+			</Routes>
+			<Footer />
+		</BrowserRouter>
 	);
 }
 
