7c54ad212a29b6f14ac85f8e57aaeb8be7cca7bb
AhmadOthman <trollingosman101@outlook.com>
2023-12-30 12:32:24 +0200

    react project

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-sequences */
+import React from "react";
+import TestingClass from "./components/TestingClass";
+import TestingFunction from "./components/TestingFunction";
+import Counter from "./components/Counter";
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<>
+				{/* <TestingFunction Name="Ahmad" />
+				<TestingFunction Name="Ali" />
+				<TestingClass Name="Othman" /> */}
+				<Counter />
+				<TestingFunction Name="ahmad"/>
+			</>
+		);
+	}
+}
+
+{
+	let arr = [1, 2, 3];
+	arr.filter((item) => {
+		return item % 2 === 0
+	})
+}
