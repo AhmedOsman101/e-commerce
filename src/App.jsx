b1de5276864685475dd484a8f6bd571f1126d655
AhmadOthman <trollingosman101@outlook.com>
2023-12-30 17:42:45 +0200

    fixed routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Nav from "./components/Nav";
-import Footer from "./components/Footer";
+
 function App() {
 	return (
-		<BrowserRouter>
-			<Nav />
-			<Routes>
-				<Route path="/" element={<App />} />
-			</Routes>
-			<Footer />
-		</BrowserRouter>
+		<>
+			<h1>Hello From App.jsx</h1>
+		</>
 	);
 }
 
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,7 +1,6 @@
-
 function Nav() {
 	return (
-		<>	
+		<>
 			<h1>Hello Nav</h1>
 		</>
 	);
